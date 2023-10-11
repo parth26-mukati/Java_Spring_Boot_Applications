f68d399e9fa3b4d9cf52e8c571c3f3b43fb52c5e
parth26-mukati <parthmukati26@gmail.com>
2023-10-11 11:05:39 +0530

    added new project

---
+++ src/main/java/com/example/repo/EmployeeRepo.java
@@ -0,0 +1,14 @@
+package com.example.repo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.entity.EmployeeEntity;
+
+public interface EmployeeRepo extends CrudRepository<EmployeeEntity, String>{
+
+}
+
+                            
+
+
+
